5aace277a31e000620e7c044b78625881f5eb653
Brad Jensen <brad@fintechnav.com>
2025-05-05 08:55:41 -0400

    reverting

--- js/components/Timeline/data/index.js
+++ js/components/Timeline/data/index.js
@@ -1,9 +1,9 @@
 const timelineItems = [
-    { ...fiskaData, ...metadata.fiska },
-    { ...vestaData, ...metadata.vesta },
-    { ...shift4Data, ...metadata.shift4 },
-    { ...globalPaymentsData, ...metadata.globalPayments },
-    { ...abancoData, ...metadata.abanco },
-    { ...goSoftwareData, ...metadata.goSoftware },
-    { ...unionCampData, ...metadata.unionCamp }
-  ];
+  { ...fiskaData, ...metadata.fiska },
+  { ...vestaData, ...metadata.vesta },
+  { ...shift4Data, ...metadata.shift4 },
+  { ...globalPaymentsData, ...metadata.globalPayments },
+  { ...abancoData, ...metadata.abanco },
+  { ...goSoftwareData, ...metadata.goSoftware },
+  { ...unionCampData, ...metadata.unionCamp }
+];
