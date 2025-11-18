34155f19cb142d1a3d61e476b27db0dbfa84c59e
FinTechNav <brad@fintechnav.com>
2025-11-18 16:07:25 -0500

    updated fiska and last updated

--- js/components/Timeline/data/companies/fiska.js
+++ js/components/Timeline/data/companies/fiska.js
@@ -9,12 +9,12 @@ const fiskaData = {
     },
   },
   description:
-    "This is where I currently hang my professional hat. In this 5-person company, I've been able to:",
+    'This is where I most recently hung my professional hat. In this 5-person company, I:',
   highlights: [
-    'Architect <a href="https://docs.integratedcommerce.io/#/" target="_blank" style="color: #c9a15f; text-decoration: none;">white-labeled payment middleware</a> that allows ISVs to quickly add card present capabilities to their cloud-based POS systems, now deployed across 1,310+ payment lanes for 980+ merchants',
-    'Build an <a href="https://fiska.com/developers/" target="_blank" style="color: #c9a15f; text-decoration: none;">Omni-Channel payment gateway</a> supporting both in-store and online transactions, launched in Q2 of 2025',
-    'Grow our monthly payment processing volume by 213% (while keeping those response times lightning fast)',
+    'Architected <a href="https://docs.integratedcommerce.io/#/" target="_blank" style="color: #c9a15f; text-decoration: none;">white-labeled payment middleware</a> that allows ISVs to quickly add card present capabilities to their cloud-based POS systems, now deployed across 1,310+ payment lanes for 980+ merchants',
+    'Built an <a href="https://fiska.com/developers/" target="_blank" style="color: #c9a15f; text-decoration: none;">Omni-Channel payment gateway</a> supporting both in-store and online transactions, launched in Q2 of 2025',
+    'Grew our monthly payment processing volume by 213% (while keeping those response times lightning fast)',
   ],
   conclusion:
-    "It's a small startup environment where I get to be both a strategic product leader and hands-on technical implementer – sometimes holding strategy sessions in the morning and answering integration questions from ISVs in the afternoon!",
+    'Fiska a small startup environment where I got to be both a strategic product leader and hands-on technical implementer – sometimes holding strategy sessions in the morning and answering integration questions from ISVs in the afternoon!',
 };
