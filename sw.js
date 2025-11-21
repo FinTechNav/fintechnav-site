7e8886d15eadf53b022ce4214402573fe9a54e29
FinTechNav <brad@fintechnav.com>
2025-11-21 18:03:10 -0500

    add menu; add terminal status; v1.2

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'heavy-pour-v1';
+const CACHE_NAME = 'heavy-pour-v1.1';
 const urlsToCache = ['/', '/index.html', '/manifest.json'];
 
 self.addEventListener('install', (event) => {
